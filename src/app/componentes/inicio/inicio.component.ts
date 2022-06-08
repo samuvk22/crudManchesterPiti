858d1102a03b3df8d3d3099a46d3546b6057f275
[samuvk22] <[gilsamuel685@gmail.com]>
2022-06-08 17:09:46 +0200

    sesions

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AnyForUntypedForms } from '@angular/forms';
+//import { AnyForUntypedForms } from '@angular/forms';
 
 
 import { InicioService } from 'src/app/servicios/inicio.service';
--- src/app/componentes/listar-noticias/listar-noticias.component.ts
+++ src/app/componentes/listar-noticias/listar-noticias.component.ts
@@ -26,6 +26,80 @@ export class ListarNoticiasComponent implements OnInit {
     });
   }
 
+
+  ngAfterViewInit():void{
+
+
+    setTimeout(cargarSesions,500);
+
+
+    function cargarSesions(){
+     
+
+
+      if(sessionStorage.getItem("sesion") == "OK" && sessionStorage.getItem("rol") == "admin") {
+        console.log("entraste al if admin");
+
+
+        for(var i = 0; i < document.getElementsByClassName("admin").length; i++){
+
+
+          console.log("entraste al for");
+
+          document.getElementsByClassName("admin")[i].classList.add("d-block");
+
+          console.log(document.getElementsByClassName("admin")[i].classList);
+
+          
+    
+        } 
+      }
+
+      if(sessionStorage.getItem("sesion") == "OK" && sessionStorage.getItem("rol") == "usuario"){
+
+        console.log("entraste al if usuario");
+
+        var admin = document.getElementsByClassName("admin");
+
+        for(var i = 0; i < admin.length; i++){
+
+
+          console.log("entraste al for");
+
+          admin[i].classList.add("d-none");
+
+          console.log(admin[i].classList);
+
+          
+    
+        } 
+
+      }
+
+      if(sessionStorage.getItem("sesion") == "NOOK" || sessionStorage.getItem("rol") == undefined){
+        
+        console.log("entraste al if sinusuario");
+
+        for(var i = 0; i < document.getElementsByClassName("admin").length; i++){
+
+
+          console.log("entraste al for");
+
+          document.getElementsByClassName("admin")[i].classList.add("d-none");
+
+          console.log(document.getElementsByClassName("admin")[i].classList);
+
+          
+    
+        } 
+
+      }
+    }
+    
+
+
+  }
+
   borrarRegistro(id:any,iControl:any){
     console.log(id);
     console.log(iControl);
